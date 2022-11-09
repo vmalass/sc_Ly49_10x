fae2bcc8bc08f3ffb8bccba7b3119180853cadec
victor <victor.malassigne@inserm.fr>
2022-11-09 13:58:25 +0100

    :maj clustree and start DE because may be problem -> work in progress

--- script/start_ana.R
+++ script/start_ana.R
@@ -45,7 +45,7 @@ DimPlot(Singlet_norm,
         cols = my_palette)+
   labs(color = "legend title")+
   ggtitle(label = "PCA")
-### Selcted 13 CP
+### Selcted 11 CP
 
 #UMAP----
 Singlet_norm <- RunUMAP(Singlet_norm, dims = 1:11, assay = "SCT")
@@ -66,16 +66,16 @@ head(Singlet_norm[[]])
 clustree(Singlet_norm, prefix = "SCT_snn_res.")
 
 ###   choose resolution 0.3
-Singlet_norm <- FindNeighbors(Singlet_norm, dims = 1:11, verbose = FALSE)
-Singlet_norm <- FindClusters(Singlet_norm, resolution = 0.2)
-Singlet_norm <- RunUMAP(Singlet_norm, dims = 1:11, assay = "SCT")
+# Singlet_norm <- FindNeighbors(Singlet_norm, dims = 1:11, verbose = FALSE)
+# Singlet_norm <- FindClusters(Singlet_norm, resolution = 0.2)
+# Singlet_norm <- RunUMAP(Singlet_norm, dims = 1:11, assay = "SCT")
+# 
+# DimPlot(Singlet_norm, 
+#         reduction = "umap", 
+#         pt.size = 1)+
+#   ggtitle(label = "UMAP resolution : 0.2")
 
-DimPlot(Singlet_norm, 
-        reduction = "umap", 
-        pt.size = 1)+
-  ggtitle(label = "UMAP resolution : 0.2")
-
-reso = c(0.3, 0.4, 0.5, 0.6, 0.7, 0.8)
+reso = c(0.3) #, 0.4, 0.5, 0.6, 0.7, 0.8
 for (i in reso) {
   Singlet_norm <- FindNeighbors(Singlet_norm, dims = 1:11, verbose = FALSE)
   Singlet_norm <- FindClusters(Singlet_norm, resolution = i)
@@ -88,5 +88,78 @@ for (i in reso) {
     ggtitle(label = paste0( "UMAP resolution : " , i , " "))
   print(a)
 }
+
+
+#####
+condition <- as.data.frame(Singlet_norm@meta.data[["HTO_maxID"]]) #creer un df de tous tes idividus
+Singlet_norm[["orig.ident"]] <- condition$`Singlet_norm@meta.data[["HTO_maxID"]]`  #on passe la 1er colonne
+Idents(Singlet_norm) <- condition$`Singlet_norm@meta.data[["HTO_maxID"]]`  # reviens a faire un levels
+levels(Singlet_norm)
+####
+
+levels(Singlet_norm)
+# test2 <- FindMarkers(Singlet_norm, 
+#                      ident.1 = "Ly49p-CD8ab", 
+#                      ident.2 = "Ly49p-CD8aa",
+#                      min.pct = 0.1, 
+#                      logfc.threshold = log(2))
+# test3 <- FindMarkers(Singlet_norm, 
+#                      ident.1 = "VV-Ly49p-CD8aa", 
+#                      ident.2 = "Ly49p-CD8aa",
+#                      min.pct = 0.1, 
+#                      logfc.threshold = log(2))
+
+Singlet_norm.markers <- FindAllMarkers(Singlet_norm, 
+                                       only.pos = F, 
+                                       min.pct = 0.1, 
+                                       logfc.threshold = log(2))
+Singlet_norm.markers %>%
+  group_by(cluster) %>%
+  slice_max(n = 2, order_by = avg_log2FC)
+
+Singlet_norm.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 30, wt = avg_log2FC) -> top10
+DoHeatmap(Singlet_norm, features = top10$gene) + NoLegend()
+
+reso = c(0.5)
+for (i in reso) {
+  Singlet_norm <- FindNeighbors(Singlet_norm, dims = 1:11, verbose = FALSE)
+  Singlet_norm <- FindClusters(Singlet_norm, resolution = i)
+  Singlet_norm <- RunUMAP(Singlet_norm, dims = 1:11, assay = "SCT")
+  
+  a<-DimPlot(Singlet_norm, 
+             reduction = "umap", 
+             pt.size = 1,
+             label = T)+
+    ggtitle(label = paste0( "UMAP resolution : " , i , " "))
+  print(a)
+}
+levels(Singlet_norm)
+
+Singlet_norm.markers <- FindAllMarkers(Singlet_norm, 
+                                       only.pos = F, 
+                                       min.pct = 0.1, 
+                                       logfc.threshold = log(2))
+Singlet_norm.markers %>%
+  group_by(cluster) %>%
+  slice_max(n = 2, order_by = avg_log2FC)
+
+Singlet_norm.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 10, wt = avg_log2FC) -> top10
+DoHeatmap(Singlet_norm, features = top10$gene) + NoLegend()+
+  ggtitle(label = "resolution : 0.5")
+
+
+
+
+
+
+
+
+
+
+
 
 
